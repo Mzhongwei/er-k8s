bfd45b4696a42ceea9495f5307b68e1c73044998
Kevin Oulai <koulai001@univ-pau.fr>
2026-06-18 16:48:54 +0200

    fixing data paths

--- k8s/scripts/sync-bert-data-pvc.sh
+++ k8s/scripts/sync-bert-data-pvc.sh
@@ -8,14 +8,14 @@ fi
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT_DIR=""
+ROOT_DIR="$(cd "${SCRIPT_DIR}/../.." && pwd)"
 
 NAMESPACE="${EAER_DATA_NAMESPACE:-argo}"
 PVC_NAME="${EAER_BERT_DATA_PVC:-pipeline-data-claim}"
-LOCAL_BERT_DATA_DIR="${ROOT_DIR}/srv/shared/data/exp_datasets/4-1_dirty_dblp_acm"
-GROUND_TRUTH_FILE="${ROOT_DIR}/srv/shared/data/exp_datasets/4-1_dirty_dblp_acm/matches.txt"
-FODORS_TABLE_A_FILE="${ROOT_DIR}/srv/shared/data/exp_datasets/4-1_dirty_dblp_acm/tableA.csv"
-FODORS_TABLE_B_FILE="${ROOT_DIR}/srv/shared/data/exp_datasets/4-1_dirty_dblp_acm/tableB.csv"
+LOCAL_BERT_DATA_DIR="/srv/shared/data/exp_datasets/4-1_dirty_dblp_acm"
+GROUND_TRUTH_FILE="/srv/shared/data/exp_datasets/4-1_dirty_dblp_acm/matches.txt"
+FODORS_TABLE_A_FILE="/srv/shared/data/exp_datasets/4-1_dirty_dblp_acm/tableA.csv"
+FODORS_TABLE_B_FILE="$/srv/shared/data/exp_datasets/4-1_dirty_dblp_acm/tableB.csv"
 PVC_MANIFEST="${ROOT_DIR}/k8s/pvc-manifests/pvc-bert-data.yaml"
 SYNC_POD="data-sync-$$"
 
